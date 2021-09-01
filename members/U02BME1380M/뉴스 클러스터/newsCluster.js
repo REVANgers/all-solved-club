1d23938bafcc63ecf6a2b6af9cd6ab7a902a37e3
June <khsj15@khu.ac.kr>
2021-09-01 18:54:03 +0900

    solved : 모음사전 , J 박성준

--- members/U02BME1380M/뉴스 클러스터/newsCluster.js
+++ members/U02BME1380M/뉴스 클러스터/newsCluster.js
@@ -7,27 +7,25 @@ function solution(str1, str2) {
 }
 
 const spliter = (str) => {
-	const arr1 = [...Array(str.length - 1)]
+	return [...Array(str.length - 1)]
 		.map((elem, idx) => {
 			const a = str[idx].toLowerCase();
 			const b = str[idx + 1].toLowerCase();
-			return `${a}${b}`;
+			return a + b;
 		})
 		.filter((elem) => {
 			return elem.match(/[a-z]{2}/) !== null;
 		});
-	return arr1;
 };
 
 const intersect = (arr1, arr2) => {
 	const result = arr1.filter((elem) => {
 		return arr2.includes(elem);
 	});
-	const set = new Set(result);
+	const set = Array.from(new Set(result));
 	return set;
 };
-const getNumberOfDuplicateSet = (intersect, arr1, arr2) => {
-	const set = Array.from(intersect);
+const getNumberOfDuplicateSet = (set, arr1, arr2) => {
 	return set.reduce((acc, cur) => {
 		const l1 = arr1.filter((elem) => cur === elem).length;
 		const l2 = arr2.filter((elem) => cur === elem).length;
