d10e845da8a903bf3d2d88c6aab0b434da622704
June <khsj15@khu.ac.kr>
2021-09-03 21:17:08 +0900

    solved : 행렬 테두기 회전하기

---
+++ members/U02BME1380M/행렬회전/rotation.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> getArray(int rows,int columns){
+    vector<vector<int>> arr;
+    int n = 1;
+    for(int i=0; i<rows; i++){
+        vector<int> row;
+        for(int j=0; j<columns; j++){
+            row.push_back(n++);
+        }
+        arr.push_back(row);
+    }
+    return arr;
+}
+
+
+
+void showArray(const vector<vector<int>> &arr){
+    const int rows = arr.size();
+    const int cols = arr[0].size();
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            cout << arr[i][j] <<"\t";
+         }
+        cout<<"\n";
+    }
+}
+int rotateArray(vector<vector<int>> &arr, const vector<int> &query){
+    int s_row = query[0]-1;
+    int s_col = query[1]-1;
+    int e_row = query[2]-1;
+    int e_col = query[3]-1;
+    cout << s_row << "," << s_col <<","<<e_row<<","<<e_col<<"\n";
+    int prev = arr[s_row][s_col];
+    int temp;
+    int min = prev;
+    for(int i=s_col; i<=e_col; i++){
+        temp = arr[s_row][i];
+        arr[s_row][i] = prev;
+        prev = temp;
+        min = min > temp ? temp : min;
+    }
+    for(int i=s_row+1; i<=e_row; i++){
+        temp = arr[i][e_col];
+        arr[i][e_col] = prev;
+        prev = temp;
+        min = min > temp ? temp : min;
+    }
+    for(int i=e_col-1; i>=s_col; i--){
+        temp = arr[e_row][i];
+        arr[e_row][i] = prev;
+        prev = temp;
+    }
+    for(int i=e_row-1; i>=s_row; i--){
+        temp = arr[i][s_col];
+        arr[i][s_col] = prev;
+        prev = temp;
+        min = min > temp ? temp : min;
+    }
+}
+
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    vector<vector<int>> arr = getArray(rows,columns);
+    for(vector<int> query : queries){
+        answer.push_back(rotateArray(arr,query));
+        showArray(arr);
+    }
+    return answer;
+}
